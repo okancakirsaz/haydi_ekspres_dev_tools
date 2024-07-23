f1aefff9e396dd7680d90c8dcd76327b70f72b3a
okancakirsaz <ocakirsaz@gmail.com>
2024-07-23 06:46:10 +0300

    comment model updated

--- lib/models/comment_model.dart
+++ lib/models/comment_model.dart
@@ -2,6 +2,7 @@ class CommentModel {
   String comment;
   String restaurantId;
   String menuId;
+  String uid;
   String customerId;
   int like;
 
@@ -9,6 +10,7 @@ class CommentModel {
     required this.comment,
     required this.like,
     required this.menuId,
+    required this.uid,
     required this.customerId,
     required this.restaurantId,
   });
@@ -17,6 +19,7 @@ class CommentModel {
     return {
       'comment': comment,
       'menuId': menuId,
+      'uid': uid,
       'customerId': customerId,
       'restaurantId': restaurantId,
       'like': like,
@@ -27,6 +30,7 @@ class CommentModel {
     return CommentModel(
       comment: json['comment'] as String,
       menuId: json['menuId'] as String,
+      uid: json['uid'] as String,
       customerId: json['customerId'] as String,
       restaurantId: json['restaurantId'] as String,
       like: json['like'] as int,
